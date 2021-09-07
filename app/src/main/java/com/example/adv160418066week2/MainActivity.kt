317d417c11d60197d5c514f21c722a939cd75ec6
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-09-08 06:05:54 +0700

    sampe week 3 (navigation, drawer)

--- app/src/main/java/com/example/adv160418066week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160418066week2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.adv160418066week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController,drawerLayout)
+        NavigationUI.setupWithNavController(navView,navController)
+        bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+       return NavigationUI.navigateUp(navController,drawerLayout)||super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/adv160418066week2/OptionFragment.kt
@@ -0,0 +1,18 @@
+package com.example.adv160418066week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class OptionFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_option, container, false)
+    }
+
+}
